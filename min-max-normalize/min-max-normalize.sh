8146be19c5a6fb2de3d2f9b2f8997afaf4643c18
baswanth-oruganti <baswanth.oruganti@gmail.com>
2021-01-27 16:02:46 +0100

    script for finding min,max and normalization

---
+++ min-max-normalize/min-max-normalize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+awk 'NR==2{max=$2}; $1!="#!"{if ($1!="#!" && max<$2) max=$2};END{printf "max=%.2f\n",max}' COLVAR
+awk 'NR==2{min=$2}; $1!="#!"{if ($1!="#!" && min>$2) min=$2};END{printf "min=%.2f\n",min}' COLVAR
+
+
+# min-max scaling or Normalization
+
+max=71.0
+min=0.0
+awk '$1!="#!" {print}' COLVAR | awk -v "maxi=$max" -v "mini=$min" '{printf "%4.2f\t%4.2f\n",($2-mini)/(maxi-mini),$3}' > normalized.txt
+
